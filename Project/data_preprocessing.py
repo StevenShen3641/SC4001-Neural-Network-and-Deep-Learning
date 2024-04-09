dd9a1686df86e190990c53688297ffa50cfd2548
StevenShen3641 <stevenshen3641@outlook.com>
2024-04-09 17:36:37 +0800

    modify

--- Project/data_preprocessing.py
+++ Project/data_preprocessing.py
@@ -1,17 +1,9 @@
 import logging
 import os
 import pickle
-import string
-
-import gensim.downloader as api
 import nltk
 import pandas as pd
-from nltk import word_tokenize
-
-from utils import remove_special_content, preprocess, replace_punct
-
 nltk.download('stopwords')
-import numpy as np
 import torch
 import torch.nn.functional as F
 from datasets import load_dataset
@@ -38,22 +30,6 @@ def load_IMDB():
     return df
 
 
-def vectorize(sentences, model, dimension, wv_type='zero_padding'):
-    tokens = [word_tokenize(s.lower()) for s in sentences]
-    wvs = []
-    for t in tokens:
-        wv = []
-        for w in t:
-            if wv_type == 'zero_padding':
-                try:
-                    wv.append(model[w])
-                except KeyError:
-                    wv.append(np.zeros(dimension))
-        wvs.append(wv)
-
-    return wvs
-
-
 def onehot(sentences, dimension):
     alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{} "
     wvs = []
@@ -70,77 +46,6 @@ def onehot(sentences, dimension):
     wvs = torch.stack(wvs)
     wvs = torch.transpose(wvs, 1, 2)
     return wvs
-
-
-def vectorize_dataset(model_type='glove-wiki-gigaword-200', name='sst2', remove=False):
-    if name == 'sst2':
-        train_data, train_labels, test_data, test_labels, dev_data, dev_labels = load_sst()
-        df_train = pd.DataFrame({'content': train_data, 'sentiment': train_labels})
-        df_val = pd.DataFrame({'content': dev_data, 'sentiment': dev_labels})
-        df_test = pd.DataFrame({'content': test_data, 'sentiment': test_labels})
-        if remove:
-            remove_special_content(df_train)
-            remove_special_content(df_val)
-            remove_special_content(df_test)
-            punct = word_tokenize(string.punctuation) + ['``', '...', '..', '\'s', '--', '-', 'n\'t', '\'', '(', ')',
-                                                         '[',
-                                                         ']', '{', '}']
-            train = preprocess(df_train['content']).map(lambda x: '. '.join(x)).map(lambda x: replace_punct(x, punct))
-            dev = preprocess(df_val['content']).map(lambda x: '. '.join(x)).map(lambda x: replace_punct(x, punct))
-            test = preprocess(df_test['content']).map(lambda x: '. '.join(x)).map(lambda x: replace_punct(x, punct))
-
-            train_data = train.to_list()
-            dev_data = dev.to_list()
-            test_data = test.to_list()
-
-        try:
-            model = api.load(model_type)
-            dimension = int(model_type.split('-')[-1])
-        except Exception as e:
-            logging.warning(f"An error occurred: {e}")
-            exit(1)
-        for p in wv_path:
-            if not os.path.exists(p + f'_{model_type}_{name}.pkl'):
-                for path in wv_path:
-                    os.makedirs(os.path.dirname(path), exist_ok=True)
-                train_wv = vectorize(train_data, model, dimension)
-                test_wv = vectorize(test_data, model, dimension)
-                dev_wv = vectorize(dev_data, model, dimension)
-                with open(wv_path[0] + f'_{model_type}_{name}.pkl', 'wb') as f:
-                    pickle.dump([train_wv, train_labels], f)
-                with open(wv_path[1] + f'_{model_type}_{name}.pkl', 'wb') as f:
-                    pickle.dump([test_wv, test_labels], f)
-                with open(wv_path[2] + f'_{model_type}_{name}.pkl', 'wb') as f:
-                    pickle.dump([dev_wv, dev_labels], f)
-                break
-        else:
-            logging.info('Vector pickle files already exists!')
-    elif name == 'IMDB':
-        df = load_IMDB()
-        labels = df['sentiment'].to_list()
-        if remove:
-            remove_special_content(df)
-            punct = word_tokenize(string.punctuation) + ['``', '...', '..', '\'s', '--', '-', 'n\'t', '\'', '(', ')',
-                                                         '[',
-                                                         ']', '{', '}']
-            data = preprocess(df['review']).map(lambda x: '. '.join(x)).map(lambda x: replace_punct(x, punct))
-            data = data.to_list()
-        else:
-            data = df['review'].to_list()
-        try:
-            model = api.load(model_type)
-            dimension = int(model_type.split('-')[-1])
-        except Exception as e:
-            logging.warning(f"An error occurred: {e}")
-            exit(1)
-        p = wv_path[1]
-        if not os.path.exists(p + f'_{model_type}_{name}.pkl'):
-            os.makedirs(os.path.dirname(p), exist_ok=True)
-            data_wv = vectorize(data, model, dimension)
-            with open(wv_path[1] + f'_{model_type}_{name}.pkl', 'wb') as f:
-                pickle.dump([data_wv, labels], f)
-        else:
-            logging.info('Vector pickle files already exists!')
 
 
 def onehot_sst2():
@@ -166,10 +71,7 @@ def onehot_sst2():
 
 
 def main():
-    model_type = 'word2vec-google-news-300'
-    # vectorize_dataset(model_type, name='sst2')
-    vectorize_dataset(model_type, name='IMDB')
-    # onehot_sst2()
+    onehot_sst2()
 
 
 if __name__ == '__main__':
